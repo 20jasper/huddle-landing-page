11f9709ccf8ec5b4af4315f210c3b7caea7ddcab
Jacob Asper <jacobasper191@gmail.com>
2022-07-20 17:17:14 -0400

    added content

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+export function Hero() {
+	return (
+		<section className="hero">
+			<header>
+				<h1 className="hero-h1">Build The Community Your Fans Will Love</h1>
+			</header>
+			<p className="hero-p">  Huddle re-imagines the way we build communities. You have a voice, but so does
+				your audience. Create connections with your users as you engage in genuine discussion. </p>
+			<button className="main--button">Get Started For Free</button>
+		</section>
+	);
+}
+
+
+
+
