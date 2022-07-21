5d93288bac8b0efe9714f23fcb536f3795e07a65
Jacob Asper <jacobasper191@gmail.com>
2022-07-21 14:34:45 -0400

    changed image source

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,16 +1,17 @@
 import { ContentSection } from "./ContentSection";
 
+import growTogether from "../images/illustration-grow-together.svg"
 
 
 export function Content() {
 	return (
 		<section className="content">
 			<ContentSection
-				imageSource="./images/illustration-grow-together.svg"
+				imageSource={growTogether}
 				imageAlt="3 pink people standing in a line"
 				boldText="Grow Together"
 				regularText="Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form."
-			// style="backgroundImage: url(./images/icon-email.svg) bottom no-repeat;"
+			// style="backgroundImage: url(../images/icon-email.svg) bottom no-repeat;"
 			/>
 		</section>
 	);
