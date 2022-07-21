90c90874259e195778e0f4450c43197fdbac9b08
Jacob Asper <jacobasper191@gmail.com>
2022-07-20 21:46:52 -0400

    positioned statistics sections

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -2,10 +2,12 @@ export function Statistics({ imageSource, imageAlt, boldText, regularText }) {
 	return (
 		<section className="statistics">
 			<img className="statistics--img" src={imageSource} alt={imageAlt} />
-			<header>
-				<h2 className="statistics--h2">{boldText}</h2>
-			</header>
-			<span>{regularText}</span>
+			<section className="statistics--textContainer">
+				<header>
+					<h2 className="statistics--h2">{boldText}</h2>
+				</header>
+				<span>{regularText}</span>
+			</section>
 		</section>
 	);
 }
--- src/components/StatisticsContainer.js
+++ src/components/StatisticsContainer.js
@@ -5,9 +5,15 @@ export function StatisticsContainer() {
 		<section className="statisticsContainer">
 			<Statistics
 				imageSource="./images/icon-communities.svg"
-				imageAlt="idk"
+				imageAlt="3 pink people standing in a line"
 				boldText="1.4k+"
 				regularText="Communities Formed"
+			/>
+			<Statistics
+				imageSource="./images/icon-messages.svg"
+				imageAlt="pink message bubble"
+				boldText="2.7m+"
+				regularText="Messages Sent"
 			/>
 		</section>
 	);
