0b6bd0dfac69cda73c446235279ffb02fe1df2f2
Jacob Asper <jacobasper191@gmail.com>
2022-07-21 14:26:20 -0400

    change image source

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,9 @@
+import logo from "../images/logo.svg"
+
 export function NavBar() {
 	return (
 		<nav className="nav">
-			<img className="nav--img" src={"images/logo.svg"} alt="Huddle Logo" />
+			<img className="nav--img" src={logo} alt="Huddle Logo" />
 			<button className="nav--button">Try It Free</button>
 		</nav>
 	);
