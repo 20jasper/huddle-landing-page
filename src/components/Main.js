167a165fbdb04b317217b11495e9d1dedd5e0c6e
Jacob Asper <jacobasper191@gmail.com>
2022-07-21 14:27:43 -0400

    removed header

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,6 @@ export function Main() {
 	return (
 		<main>
 			<Hero />
-			<header>
-				<h2 className="hero-h1">Ready To Build Your Community?</h2>
-			</header>
-			<MainButton />
 		</main>
 	);
 }
