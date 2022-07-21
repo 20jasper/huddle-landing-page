7e47ffc023179a5e60b6b13080c62622e8e90857
Jacob Asper <jacobasper191@gmail.com>
2022-07-21 15:33:14 -0400

    change images and alt text

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,6 +1,9 @@
 import { ContentSection } from "./ContentSection";
 
 import growTogether from "../images/illustration-grow-together.svg"
+import flowingConversation from "../images/illustration-flowing-conversation.svg"
+import yourUsers from "../images/illustration-your-users.svg"
+
 
 import bgSectionTopMobile1 from "../images/bg-section-top-mobile-1.svg"
 
@@ -9,21 +12,21 @@ export function Content() {
 		<section className="content">
 			<ContentSection
 				imageSource={growTogether}
-				imageAlt="3 pink people standing in a line"
+				imageAlt="2 business people standing in front of an abstract website"
 				boldText="Grow Together"
 				regularText="Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form."
-			// style={{ backgroundImage: `url(${bgSectionTopMobile1});` }}
+			// style={{ backgroundImage: `url(${bgSectionTopMobile1}) no-repeat 0% -50%` }}
 			/>
 			<ContentSection
-				imageSource={growTogether}
-				imageAlt="3 pink people standing in a line"
+				imageSource={flowingConversation}
+				imageAlt="4 people sitting down and talking to eachother at a table"
 				boldText="Flowing Conversations"
 				regularText="You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow."
 			// style={{ backgroundImage: `url(${bgSectionTopMobile1});` }}
 			/>
 			<ContentSection
-				imageSource={growTogether}
-				imageAlt="3 pink people standing in a line"
+				imageSource={yourUsers}
+				imageAlt="3 people standing up and interacting with speech bubbles"
 				boldText="Your Users"
 				regularText="It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately."
 			// style={{ backgroundImage: `url(${bgSectionTopMobile1});` }}
