8a225cf237e47bec56499009442ef8f231b01d3e
Jacob Asper <jacobasper191@gmail.com>
2022-07-21 14:26:48 -0400

    added content

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './index.css';
 import { NavBar } from './components/NavBar';
 import { Main } from './components/Main';
+import { Content } from "./components/Content";
+import { MainButton } from './components/MainButton';
 
 const mobileBreakPoint = "375px"
 
@@ -9,6 +11,13 @@ function App() {
 		<div className="App">
 			<NavBar />
 			<Main />
+			<Content
+				mobileBreakPoint={mobileBreakPoint}
+			/>
+			<header>
+				<h2 className="hero-h1">Ready To Build Your Community?</h2>
+			</header>
+			<MainButton />
 		</div>
 	);
 }
