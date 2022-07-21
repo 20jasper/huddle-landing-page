040d539266cc2f681c96354064598d729ed43847
Jacob Asper <jacobasper191@gmail.com>
2022-07-21 14:30:02 -0400

    updated image sources

--- src/components/StatisticsContainer.js
+++ src/components/StatisticsContainer.js
@@ -1,16 +1,20 @@
 import { Statistics } from "./Statistics";
 
+import pinkPeople from "../images/icon-communities.svg"
+import messages from "../images/icon-messages.svg"
+
+
 export function StatisticsContainer() {
 	return (
 		<section className="statisticsContainer">
 			<Statistics
-				imageSource="./images/icon-communities.svg"
+				imageSource={pinkPeople}
 				imageAlt="3 pink people standing in a line"
 				boldText="1.4k+"
 				regularText="Communities Formed"
 			/>
 			<Statistics
-				imageSource="./images/icon-messages.svg"
+				imageSource={messages}
 				imageAlt="pink message bubble"
 				boldText="2.7m+"
 				regularText="Messages Sent"
