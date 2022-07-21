692b704f703b953ac2caa7ac2734b35dc5139985
Jacob Asper <jacobasper191@gmail.com>
2022-07-21 14:59:36 -0400

    added more sections and updated text

--- src/components/Content.js
+++ src/components/Content.js
@@ -14,6 +14,20 @@ export function Content() {
 				regularText="Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form."
 			// style={{ backgroundImage: `url(${bgSectionTopMobile1});` }}
 			/>
+			<ContentSection
+				imageSource={growTogether}
+				imageAlt="3 pink people standing in a line"
+				boldText="Flowing Conversations"
+				regularText="You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow."
+			// style={{ backgroundImage: `url(${bgSectionTopMobile1});` }}
+			/>
+			<ContentSection
+				imageSource={growTogether}
+				imageAlt="3 pink people standing in a line"
+				boldText="Your Users"
+				regularText="It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately."
+			// style={{ backgroundImage: `url(${bgSectionTopMobile1});` }}
+			/>
 		</section>
 	);
 }
